0eee1f87d7c81239678b4e3c93ad7a9f111a2338
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-09-09 15:41:27 -0500

    finish participation 2

---
+++ VowelandConst/Part2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string word = txtbox.Text;
+            vList.Items.Clear();
+            cList.Items.Clear();
+            // doing a loop statement that would sort vowels and constant 
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
+                {
+                    vList.Items.Add(word[i]);
+                }
+                else
+                {
+                    cList.Items.Add(word[i]);
+                }
+            }
+
+            txtbox.Clear();
+
+        }
+    }
+}
